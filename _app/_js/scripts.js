29f4cf49e919f3936e04219b9e80c5727cd4fd45
pavelp21 <pavelvp2182@yandex.ru>
2020-02-08 11:47:03 +0500

    Page is typeseted

---
+++ _app/_js/scripts.js
@@ -0,0 +1,34 @@
+//@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend jquery.fancybox.min.js
+
+$(document).ready(function () {
+    /* Якорь */
+    $("a[href='#prices']").click(function (h) {
+        h.preventDefault();
+        var f = $(this).attr("href"),
+            g = $(f).offset().top;
+        $("body,html").animate({
+            scrollTop: g
+        }, 1500)
+    });
+    $(function () {
+        $("[data-fancybox]").fancybox({
+            buttons: [
+                'slideShow',
+                'share',
+                'zoom',
+                'fullScreen',
+                // 'download',
+                'close'
+            ],
+            speed: 330,
+            loop: true,
+            opacity: "auto",
+            // autoScale: true,
+            mouseWheel: true,
+            transitionEffect: 'slide'
+        });
+    });
+
+    /*Конец документа*/
+});
